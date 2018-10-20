624e9466a09a27f59238c4d69d7a02069f71f6ca
Ian Seal <iantheging@gmail.com>
2018-10-19 22:46:54 -0500

    Added helper functions

--- TySight/src/poultrysonic.cpp
+++ TySight/src/poultrysonic.cpp
@@ -7,31 +7,42 @@ const int echoPin = 10;
 // defines variables
 long duration;
 int distance;
+int count = 0;
+int distanceArr[10];
 
-void setup()
+int getDistance(int trigPin, int echoPin)
 {
-    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-    pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
-    Serial.begin(9600);       // Starts the serial communication
-}
 
-void loop()
-{
     // Clears the trigPin
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
+
     // Sets the trigPin on HIGH state for 10 micro seconds
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
+
     // Reads the echoPin, returns the sound wave travel time in microseconds
     duration = pulseIn(echoPin, HIGH);
+
     // Calculating the distance
     distance = duration * 0.034 / 2;
-    // Prints the distance on the Serial Monitor
-    Serial.print("Distance: ");
-    Serial.println(distance);
 
-    // Updated version
-    
+    return distance;
+}
+
+int errorCheck()
+{
+    if (count < 10)
+    {
+        distanceArr[count] = distance;
+        count++;
+    }
+    else
+    {
+        for (int i = 0; i < sizeof(distanceArr) - 1; i++)
+        {
+            distanceArr[i] = distanceArr[i + 1];
+        }
+    }
 }
---
+++ TySight/src/poultrysonic.h
@@ -0,0 +1,6 @@
+#ifndef POULTRYSONIC_H
+#define POULTRYSONIC_H
+
+int getDistance(int trigPin, int echoPin);
+
+#endif
