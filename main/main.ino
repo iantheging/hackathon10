9e0d833237c52d610206380cffdc8582a174b418
Ian Seal <iantheging@gmail.com>
2018-10-19 21:08:45 -0500

    Added basic loop to display distance

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+const int trigPin = 9;
+const int echoPin = 10;
 
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
 
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  distance = duration*0.034/2;
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
 }
