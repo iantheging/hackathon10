2a4d24b6a3fd900d3d31a3865d8db46bf773bf31
Evan G Timmons <evangeorgetimmons@gmail.com>
2018-10-20 04:45:16 -0500

    Auto stash before merge of "master" and "origin/master"

--- TySight/src/vibrator.cpp
+++ TySight/src/vibrator.cpp
@@ -8,7 +8,6 @@ Vibrator::Vibrator(int analogOutPin)
 void Vibrator::vibrate(int distance)
 {
     // creates a strength value for analog output
-    // inverted strength so it is stronger for closer objects
-    int strength = map(distance, 0, 50, 150, 0);
+    int strength = map(distance, 0, 100, 153, 0);
     analogWrite(analogOutPin, strength);
 }
