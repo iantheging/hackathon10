ee9c20a3ad4a926dbb5a39783798896ce83b2b4f
Evan G Timmons <evangeorgetimmons@gmail.com>
2018-10-20 01:36:06 -0500

    Added temperature values for distance

--- TySight/src/main.cpp
+++ TySight/src/main.cpp
@@ -24,18 +24,11 @@ void setup()
     // Set Temperature pins
     pinMode(dataPin, INPUT);
 
-    Serial.begin(9600);       // Starts the serial communication
+    Serial.begin(9600); // Starts the serial communication
 }
 
 void loop()
 {
-    // Create instance of Poultrysonic
-    Poultrysonic ps (trigPin, echoPin);
-    distance = ps.getDistance();
-
-    // Create instance of temperature
-    Temperature temp (dataPin, deadPin);
-    temperature = temp.getTemperatureF();
 
     /*
     // Prints the distance on the Serial Monitor
@@ -43,7 +36,23 @@ void loop()
     Serial.println(distance);
     */
 
-    // Prints the temperature in the Serial Monitor
+    // Create instance of Temperature
+    Temperature temp(dataPin, deadPin);
+    temperature = temp.getTemperatureC();
+
+    // Create instance of Poultrysonic
+    Poultrysonic ps(trigPin, echoPin, temperature);
+    distance = ps.getDistance();
+
+    // Prints the distance in the Serial Monitor
+    Serial.print("Distance: ");
+    Serial.println(distance);
+
     Serial.print("Temperature: ");
     Serial.println(temperature);
+
+    Serial.println();
+
+    // Delay to make data legible in serial
+    delay(500);
 }
